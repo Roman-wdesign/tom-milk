241deab2c9ba69841f6e548168a22511661075b0
Roman-wdesign <klauss.elephant@gmail.com>
2023-06-13 23:30:37 +0700

    router links color edited third

--- src/shared/views/GearView.vue
+++ src/shared/views/GearView.vue
@@ -2,16 +2,12 @@
   <div class="container mx-auto">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-16">
       <div class="flex justify-center my-4">
-        <RouterLink
-          class="active:text-emerald-600 text-cyan-600 active:text-blue-500"
-          to="/gear/calc"
+        <RouterLink class="active:text-emerald-600 text-cyan-600 active:opacity-75" to="/gear/calc"
           ><p>Считать обрат</p></RouterLink
         >
       </div>
       <div class="flex justify-center my-4">
-        <RouterLink
-          class="active:text-emerald-600 text-cyan-600 active:text-blue-500"
-          to="/gear/raw"
+        <RouterLink class="active:text-emerald-600 text-cyan-600 active:opacity-75" to="/gear/raw"
           ><p>Считать сырое молоко</p></RouterLink
         >
       </div>
