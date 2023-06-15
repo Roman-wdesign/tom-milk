55093b751ee98684f6ed84186c0aa0917b63804f
Roman-wdesign <klauss.elephant@gmail.com>
2023-06-15 21:05:55 +0700

    more simple links in GearView

--- src/shared/views/GearView.vue
+++ src/shared/views/GearView.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="container mx-auto">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-16">
-      <div class="flex justify-center my-4">
-        <RouterLink class="active:text-emerald-600 text-cyan-600 active:opacity-75" to="/gear/calc"
-          ><p>Считать обрат</p></RouterLink
+    <div class="flex flex-row justify-center mt-16">
+      <div class="my-4 mx-4">
+        <RouterLink
+          class="active:text-emerald-600 focus:shadow-xl text-cyan-600 active:opacity-75"
+          to="/gear/calc"
+          ><p>Обрат</p></RouterLink
         >
       </div>
-      <div class="flex justify-center my-4">
-        <RouterLink class="active:text-emerald-600 text-cyan-600 active:opacity-75" to="/gear/raw"
-          ><p>Считать сырое молоко</p></RouterLink
+      <div class="my-4 mx-4">
+        <RouterLink
+          class="active:text-emerald-600 focus:shadow-xl text-cyan-600 active:opacity-75"
+          to="/gear/raw"
+          ><p>Сырое молоко</p></RouterLink
         >
       </div>
     </div>
