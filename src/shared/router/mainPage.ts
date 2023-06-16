88790aa7573123f2064443e797dc97cfc3759e69
Roman-wdesign <klauss.elephant@gmail.com>
2023-06-16 17:49:32 +0700

    router to AboutView was deleted

--- src/shared/router/mainPage.ts
+++ src/shared/router/mainPage.ts
@@ -1,6 +1,5 @@
 const mainRoutes = [
     { path: '/', name: 'home', component: () => import('@/shared/views/HomeView.vue') },
-    { path: '/about', name: 'about', component: () => import('@/shared/views/AboutView.vue') },
 
 ]
 export default mainRoutes
