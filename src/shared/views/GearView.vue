c37906ac0e8710cfb42c79e6eaa5a70ee2afc232
Roman <gabbasov.tomsk@gmail.com>
2024-03-11 00:15:26 +0700

    menu

--- src/shared/views/GearView.vue
+++ src/shared/views/GearView.vue
@@ -16,11 +16,11 @@ const currentTab = ref(tabs[0])
 <template>
   <div class="container mx-auto px-4 ">
     <div class="flex justify-center">
-      <h1 class="text-2xl md:text-3xl lg:text-4xl mt-16">Посчитать</h1>
+      <h1 class="text-2xl sm:text-2xl md:text-3xl lg:text-4xl mt-16">Посчитать</h1>
     </div>
     <div class="flex flex-row justify-center mt-16">
       <div>
-        <button v-for="(tab, i) in tabs" :key="tab.name" class="px-8 py-2 rounded-none text-slate-700"
+        <button v-for="(tab, i) in tabs" :key="tab.name" class="px-6 sm:px-12 md:px-8 py-2 rounded-none text-slate-700"
           :class="{ 'bg-blue-300': tab.name === currentTab.name }" @click="currentTab = tabs[i]">
           {{ tab.name }}
         </button>
