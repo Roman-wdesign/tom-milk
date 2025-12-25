104b4d6f06e66fb00499e804a1452bac3889de6a
Roman <klauss.elephant@gmail.com>
2025-12-25 22:18:51 +0700

    style:h3-some

--- src/shared/views/FinnPack.vue
+++ src/shared/views/FinnPack.vue
@@ -3,6 +3,8 @@ import { ref, computed } from 'vue'
 
 const toDay = ref(new Date())
 
+
+
 function formatDateToDDMMYY(date: Date): string {
   const day = String(date.getDate()).padStart(2, '0')
   const month = String(date.getMonth() + 1).padStart(2, '0')
@@ -54,7 +56,10 @@ const computedLama = computed(() => {
     </div>
     <div class="flex-col justify-center mb-5">
       <h3 class="text-slate-700 text-decoration-line: underline">14 дней</h3>
-      <h3 class="text-slate-700">Лама, Только Молоко, Коровкино, Красная цена (кефир и молоко)</h3>
+      <h3 class="text-slate-700">Лама</h3>
+      <h3 class="text-slate-700">Только Молоко</h3>
+      <h3 class="text-slate-700">Коровкино</h3>
+      <h3 class="text-slate-700">Красная цена (кефир и молоко)</h3>
 
       <h5 class="text-slate-700 font-bold">{{ computedTomorrow.toLocaleString() }}</h5>
       <h5 class="text-slate-700 font-bold">{{ computedLama.toLocaleString() }}</h5>
